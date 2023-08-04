3ecf325baf7c11a9aeb4baed2d836a1b1cdac935
kevislin <605684854@qq.com>
2023-08-05 01:10:07 +0800

    数据迁移的代码都好了

--- auto.bat
+++ auto.bat
@@ -1,7 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
-set list[0]=baron_xin
-set list[1]=cel_seq_drop_seq
-for /l %%n in (1,1,1) do (
-    Rscript preprocess.R E:\YuAnHuang\kevislin\second_proj\scALGCN\experiment\!list[%%n]!
+
+set projects=10x_v3_drop_seq,^
+10x_v3_indrop,^
+5061_84133,^
+84133_5061,^
+84133_81608,^
+84133_85241,^
+84133_combine,^
+drop_seq_10x_v3,^
+drop_seq_seq_well,^
+indrop_10x_v3,^
+indrop_drop_seq,^
+seq_well_10x_v3,^
+seq_well_drop_seq
+
+for %%i in (%projects%) do (
+@REM     echo %%i
+    Rscript preprocess.R E:\YuAnHuang\kevislin\second_proj\scALGCN\experiment\%%i
 )
+echo File copy successfully
+
+
--- data_migration.bat
+++ data_migration.bat
@@ -1,23 +1,32 @@
 @echo off
+setlocal enabledelayedexpansion
 set ab_path=E:\YuAnHuang\kevislin\Cell_Classification\experiment\platform_v2
 set des_ab_path=E:\YuAnHuang\kevislin\second_proj\scALGCN\experiment
-set project[0]=cel_seq_10x_v3
+set projects=84133_5061,^
+84133_81608,^
+84133_85241,^
+84133_combine,^
+drop_seq_10x_v3,^
+drop_seq_seq_well,^
+indrop_10x_v3,^
+indrop_drop_seq,^
+seq_well_10x_v3,^
+seq_well_drop_seq
 
-for /l %%n in (0,1,0) do (
-    set ref_path=%ab_path%\!project[%%n]!\raw_data\ref
-    set query_path=%ab_path%\!project[%%n]!\raw_data\query
-    set des_path=%des_ab_path%\!project[%%n]!
-    set des_data_path=%des_path%\data
-    set des_raw_data_path=%des_path%\raw_data
-    if not exist %des_path% mkdir %des_path%
-    if not exist %des_data_path% mkdir %des_data_path%
-    if not exist %des_raw_data_path% mkdir %des_raw_data_path%
-
-    copy %ref_path%\data_1.csv %des_raw_data_path%\ref_data.csv
-    copy %ref_path%\label_1.csv %des_raw_data_path%\ref_label.csv
-    copy %query_path%\data_1.csv %des_raw_data_path%\query_data.csv
-    copy %query_path%\label_1.csv %des_raw_data_path%\query_label.csv
-
+for %%i in (%projects%) do (
+    set ref_path=!ab_path!\%%i\raw_data\ref
+    set query_path=!ab_path!\%%i\raw_data\query
+    set des_path=!des_ab_path!\%%i
+    set des_data_path=!des_path!\data
+    set des_raw_data_path=!des_path!\raw_data
+    if not exist !des_path! mkdir !des_path!
+    if not exist !des_data_path! mkdir !des_data_path!
+    if not exist !des_raw_data_path! mkdir !des_raw_data_path!
+    copy !ref_path!\data_1.csv !des_raw_data_path!\ref_data.csv
+    copy !ref_path!\label_1.csv !des_raw_data_path!\ref_label.csv
+    copy !query_path!\data_1.csv !des_raw_data_path!\query_data.csv
+    copy !query_path!\label_1.csv !des_raw_data_path!\query_label.csv
 )
-
 echo File copy successfully
+echo start preprocess
+auto.bat
