dd443c10aebe470b26152fdd57efcb9b0691a0c5
kevislin <605684854@qq.com>
2023-12-15 16:23:37 +0800

    解决compare_graph.py的bug

--- compare_graph.py
+++ compare_graph.py
@@ -1,29 +1,25 @@
 import pandas as pd
 
 
-def get_tuple(df):
-    # 转置DataFrame，将行变为列
-    transposed_df = df.transpose()
-    
-    graph = set()
-    # 输出每一列作为一个元组
-    for column in transposed_df.values:
-        graph.add(tuple(map(int, column)))
-    
+def get_edge_idx(df):
+    # 转置DataFrame，将行变为列          
+    row = map(int, df.iloc[0, :].tolist())
+    col = map(int, df.iloc[1, :].tolist())
+    graph = set(tuple(zip(row, col)))    
     return graph
                   
     
     
-old_graph = get_tuple(pd.read_csv('old_graph.csv'))
-new_graph = get_tuple(pd.read_csv('new_graph.csv'))
+old_graph = get_edge_idx(pd.read_csv('old_graph.csv', header=None))
+new_graph = get_edge_idx(pd.read_csv('new_graph.csv', header=None))
 
 # 新增的边
 new_edge = new_graph - old_graph
 print("新增边数{:}".format(len(new_edge)))
-print(new_edge)
+# print(new_edge)
 
 # 删除的边数
 del_edge = old_graph - new_graph
 print("删除的边数{:}".format(len(del_edge)))
-print(del_edge)
+# print(del_edge)
 
