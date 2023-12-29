6557dc18afb0f4342e0d17156be3463682c47e9d
kevislin <605684854@qq.com>
2023-12-29 17:18:08 +0800

    去掉torch_geometric

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from torch_geometric.nn import GCNConv
 import torch
 import torch.nn.functional as F
 from torch_sparse import SparseTensor
@@ -20,25 +19,6 @@ from graphtransformer.layers.mlp_readout_layer import MLPReadout
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-class GCN(torch.nn.Module):
-    '''
-        Two layer GCN network
-    '''
-
-    def __init__(self, input_dim, hidden_units, output_dim):
-        super(GCN, self).__init__()
-        torch.manual_seed(32)
-        self.conv1 = GCNConv(input_dim, hidden_units)
-        self.conv2 = GCNConv(hidden_units, output_dim)
-
-    def forward(self, x, edge_idx):
-        adj = SparseTensor(row=edge_idx[0], col=edge_idx[1], sparse_sizes=(x.shape[0], x.shape[0]))
-        x = self.conv1(x, adj.t())
-        x = x.relu()
-        # x = F.dropout(x, p=0.3, training=self.training)
-        x = self.conv2(x, adj.t())
-        return x
 
 
 class SparseMHA(nn.Module):
