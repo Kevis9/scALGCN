7303e01215b694a973fceac18a88913385a51365
kevislin <605684854@qq.com>
2024-01-09 16:05:03 +0800

    使用multioutputRegressor让svr支持多维回归任务

--- regression_methods.py
+++ regression_methods.py
@@ -3,6 +3,7 @@
 '''
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.preprocessing import StandardScaler
@@ -56,12 +57,14 @@ class Regressor():
         return (mse, mae, r2)
     
     def svr(self):
-        svr_model = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))  # 调整 C 和 epsilon 参数
-        svr_model.fit(self.X_train, self.y_train)
-        y_pred = svr_model.predict(self.X_test)
+        svr_model = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))  
+        multioutput_svr_model = MultiOutputRegressor(svr_model)
+        multioutput_svr_model.fit(self.X_train, self.y_train)
+        y_pred = multioutput_svr_model.predict(self.X_test)
         mse = mean_squared_error(y_pred, self.y_test)
         mae = mean_absolute_error(y_pred, self.y_test)
         r2 = r2_score(y_pred, self.y_test)
         print("svr mse: {:.3f}, mae: {:.3f}, r2: {:.3f}".format(mse, mae, r2))
         return (mse, mae, r2)
+        
         
