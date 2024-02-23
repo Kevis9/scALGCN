50c655ecfad2c66776127362135b402d1de217df
kevislin <605684854@qq.com>
2024-02-23 18:59:18 +0800

    【NIMP】debug

--- data_process_platforms/process_wu.py
+++ data_process_platforms/process_wu.py
@@ -47,15 +47,23 @@ meta_data = pd.read_csv(os.path.join(dir_name, 'meta_data.txt'), delimiter='\t')
 cell_names = np.array(meta_data['NAME'].tolist())
 cell_types = np.array(meta_data['CellType'].tolist())
 bio_samples = np.array(meta_data['biosample_id'].tolist())
+donor_id = np.array(meta_data['donor_id'])
+print(np.unique(donor_id))
+print(np.unique(bio_samples))
+
 csr_data = mmread(os.path.join(dir_name, 'data.mtx')).tocsr().transpose() # cell * gene
 
-group_list = ['BC-P1', 'BC-P2', 'BC-P3', 'PC-P1', ' M-P1'] #注意源文件里面的M-P1前面多了一个空格
+group_list = ['BC-P1', 'BC-P2', 'BC-P3', 'PC-P1', ' M-P1'] #注意源文件里面的M-P1前面多了一个制表符
 donor_id = ['CID4471', 'CID44971', 'CID4513', 'PID17267', 'SCC180161']
 
-for i, group in enumerate(group_list):        
+for i, group in enumerate(group_list):      
+    if i<4:
+        continue
+      
     key1 = group
     key2 = donor_id[i]    
     idx = list(np.where(np.isin(bio_samples, [key1, key2]))[0])          
+    
     data = csr_data[idx, :]    
     # BC和PC有上皮组织的cancer，全部转成cancer类型就好    
     types = cell_types[idx]
