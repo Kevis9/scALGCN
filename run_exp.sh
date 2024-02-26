27550b88995356976f83fde4d731f19ae6c3cacc
kevislin <605684854@qq.com>
2024-02-26 16:38:32 +0800

    [NIMP]projs changes

--- run_exp.sh
+++ run_exp.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-projects=("bcp1_6000-bcp2_6000-exp0013")
-        #   "bcp1_6000-bcp2_6000-exp0050" \
-        #   "bcp1_6000-bcp2_6000-exp0047" \
-        #   "bcp1_6000-bcp3_6000-exp0013" \
-        #   "bcp1_6000-bcp3_6000-exp0050" \
-        #   "bcp1_6000-bcp3_6000-exp0047" \
-        #   "bcp2_6000-bcp3_6000-exp0013" \
-        #   "bcp2_6000-bcp3_6000-exp0047" \
-        #   "bcp2_6000-bcp3_6000-exp0050" \
-        #   "bcp1_6000-pcp1_6000-exp0013" \
-        #   "bcp1_6000-pcp1_6000-exp0038" \
-        #   "bcp1_6000-pcp1_6000-exp0050" \
-        #   "bcp1_6000-pcp1_6000-exp0047" \
-        #   "bcp1_6000-mp1_6000-exp0013" \
-        #   "bcp1_6000-mp1_6000-exp0040" \
-        #   "bcp1_6000-mp1_6000-exp0050" \
-        #   "bcp1_6000-mp1_6000-exp0047")
+projects=("bcp1_6000-bcp2_6000-exp0013"
+          "bcp1_6000-bcp2_6000-exp0050" \
+          "bcp1_6000-bcp2_6000-exp0047" \
+          "bcp1_6000-bcp3_6000-exp0013" \
+          "bcp1_6000-bcp3_6000-exp0050" \
+          "bcp1_6000-bcp3_6000-exp0047" \
+          "bcp2_6000-bcp3_6000-exp0013" \
+          "bcp2_6000-bcp3_6000-exp0047" \
+          "bcp2_6000-bcp3_6000-exp0050" \
+          "bcp1_6000-pcp1_6000-exp0013" \
+          "bcp1_6000-pcp1_6000-exp0038" \
+          "bcp1_6000-pcp1_6000-exp0050" \
+          "bcp1_6000-pcp1_6000-exp0047" \
+          "bcp1_6000-mp1_6000-exp0013" \
+          "bcp1_6000-mp1_6000-exp0040" \
+          "bcp1_6000-mp1_6000-exp0050" \
+          "bcp1_6000-mp1_6000-exp0047")
 
 reverse_projs=("bcp2_6000-bcp1_6000-exp0013" \
 "bcp2_6000-bcp1_6000-exp0050" \
@@ -39,11 +39,11 @@ reverse_projs=("bcp2_6000-bcp1_6000-exp0013" \
 
 for i in ${projects[@]}; do
     # ref_query_auxilary
-    # python gt_main.py --data_dir=./experiments/$i  --use_auxilary
-    # python gt_main.py --add_pos_enc --data_dir=./experiments/$i --use_auxilary
-    # python gt_main.py --active_learning --data_dir=./experiments/$i --use_auxilary
-    # python gt_main.py --adj_training --data_dir=./experiments/$i --use_auxilary
-    # python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --data_dir=./experiments/$i  --use_auxilary
+    python gt_main.py --add_pos_enc --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --active_learning --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --adj_training --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
     
     # ref_query
     python gt_main.py --data_dir=./experiments/$i
@@ -54,18 +54,18 @@ for i in ${projects[@]}; do
 done
 
 
-# for i in ${reverse_projs[@]}; do
-#     # query-ref-auxilary
-#     python gt_main.py --data_dir=./experiments/$i  --use_auxilary
-#     python gt_main.py --add_pos_enc --data_dir=./experiments/$i --use_auxilary
-#     python gt_main.py --active_learning --data_dir=./experiments/$i --use_auxilary
-#     python gt_main.py --adj_training --data_dir=./experiments/$i --use_auxilary
-#     python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
+for i in ${reverse_projs[@]}; do
+    # query-ref-auxilary
+    python gt_main.py --data_dir=./experiments/$i  --use_auxilary
+    python gt_main.py --add_pos_enc --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --active_learning --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --adj_training --data_dir=./experiments/$i --use_auxilary
+    python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
     
-#     # query-ref
-#     python gt_main.py --data_dir=./experiments/$i
-#     python gt_main.py --add_pos_enc --data_dir=./experiments/$i
-#     python gt_main.py --active_learning --data_dir=./experiments/$i
-#     python gt_main.py --adj_training --data_dir=./experiments/$i
-#     python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i       
-# done
+    # query-ref
+    python gt_main.py --data_dir=./experiments/$i
+    python gt_main.py --add_pos_enc --data_dir=./experiments/$i
+    python gt_main.py --active_learning --data_dir=./experiments/$i
+    python gt_main.py --adj_training --data_dir=./experiments/$i
+    python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i       
+done
