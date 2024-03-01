087b4a5d4ceec182600e793dd84cff2bf3d2ef24
kevislin <605684854@qq.com>
2024-03-01 17:28:14 +0800

    [add]for r

--- other_methods/r_methods/run_r.sh
+++ other_methods/r_methods/run_r.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 dir_name=(
-    "bcp1_6000-bcp2_6000-exp0013" \
-    "bcp2_6000-bcp3_6000-exp0013" \
-    "bcp1_6000-pcp1_6000-exp0013" \
-    "bcp1_6000-mp1_6000-exp0013" \
-    "bcp2_6000-bcp1_6000-exp0013" \
-    "bcp3_6000-bcp1_6000-exp0013" \
-    "bcp3_6000-bcp2_6000-exp0013" \
-    "pcp1_6000-bcp1_6000-exp0013" \
-    "mp1_6000-bcp1_6000-exp0013" \
+    # "bcp1_6000-bcp2_6000-exp0013" \
+    # "bcp2_6000-bcp3_6000-exp0013" \
+    # "bcp1_6000-pcp1_6000-exp0013" \
+    # "bcp1_6000-mp1_6000-exp0013" \
+    # "bcp2_6000-bcp1_6000-exp0013" \
+    # "bcp3_6000-bcp1_6000-exp0013" \
+    # "bcp3_6000-bcp2_6000-exp0013" \
+    # "pcp1_6000-bcp1_6000-exp0013" \
+    # "mp1_6000-bcp1_6000-exp0013" \
+    "bcp1_6000-bcp3_6000-exp0013"
 )
 
 projs=(
-    "bcp1_6000-bcp2_6000" \
-    "bcp2_6000-bcp3_6000" \
-    "bcp1_6000-pcp1_6000" \
-    "bcp1_6000-mp1_6000" \
-    "bcp2_6000-bcp1_6000" \
-    "bcp3_6000-bcp1_6000" \
-    "bcp3_6000-bcp2_6000" \
-    "pcp1_6000-bcp1_6000" \
-    "mp1_6000-bcp1_6000" \
+    # "bcp1_6000-bcp2_6000" \
+    # "bcp2_6000-bcp3_6000" \
+    # "bcp1_6000-pcp1_6000" \
+    # "bcp1_6000-mp1_6000" \
+    # "bcp2_6000-bcp1_6000" \
+    # "bcp3_6000-bcp1_6000" \
+    # "bcp3_6000-bcp2_6000" \
+    # "pcp1_6000-bcp1_6000" \
+    # "mp1_6000-bcp1_6000" \
+    "bcp1_6000-bcp3_6000"
 )
 
 # 获取数组长度
@@ -30,5 +32,5 @@ length=${#projs[@]}
 for ((i = 0; i < length; i++)); do
    python h5_adaptor.py --dir_name=/home/Users/kevislin/scALGCN/experiments/${dir_name[i]} --proj=${projs[i]}
    Rscript r_methods.R /home/Users/kevislin/scALGCN/experiments/${dir_name[i]} ${projs[i]}
-   python decoder.py --proj=${projs[i]}
+#    python decoder.py --proj=${projs[i]}
 done
