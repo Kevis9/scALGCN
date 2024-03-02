44386e2abf2ad37b28c2c83f207bcfabd61bcaca
kevislin <605684854@qq.com>
2024-03-02 20:26:24 +0800

    [add]plot

--- plot/box_plot_cancer_exp.py
+++ plot/box_plot_cancer_exp.py
@@ -2,8 +2,8 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('al_exp.csv', sep='\t')
-sns.boxplot(data=data,x='method', y='val', hue='metric', showfliers=False)
+data = pd.read_csv('cancer_exp.csv', sep='\t')
+sns.boxplot(data=data,x='method', y='val', hue='metric', showfliers=False, width=.4)
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))    
 plt.xlabel('')
 plt.ylabel('')
