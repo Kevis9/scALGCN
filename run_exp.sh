dec5fc38b3e9e2ac04d355e69c25f49d99f75ee7
kevislin <605684854@qq.com>
2024-03-13 10:21:22 +0800

    [NIMP]change hyper-paras【】

--- run_exp.sh
+++ run_exp.sh
@@ -2,14 +2,14 @@
 
 projects=(
           "bcp1_6000-bcp2_6000-exp0013"
-        #   "bcp1_6000-bcp2_6000-exp0050" \
-        #   "bcp1_6000-bcp2_6000-exp0047" \
-        #   "bcp1_6000-bcp3_6000-exp0013" \
-        #   "bcp1_6000-bcp3_6000-exp0050" \
-        #   "bcp1_6000-bcp3_6000-exp0047" \
-        #   "bcp2_6000-bcp3_6000-exp0013" \
-        #   "bcp2_6000-bcp3_6000-exp0047" \
-        #   "bcp2_6000-bcp3_6000-exp0050" \
+          # "bcp1_6000-bcp2_6000-exp0050" \
+          # "bcp1_6000-bcp2_6000-exp0047" \
+          # "bcp1_6000-bcp3_6000-exp0013" \
+          # "bcp1_6000-bcp3_6000-exp0050" \
+          # "bcp1_6000-bcp3_6000-exp0047" \
+          # "bcp2_6000-bcp3_6000-exp0013" \
+          # "bcp2_6000-bcp3_6000-exp0047" \
+          # "bcp2_6000-bcp3_6000-exp0050" \
           # "bcp1_6000-pcp1_6000-exp0013" \
           # "bcp1_6000-pcp1_6000-exp0038" \
           # "bcp1_6000-pcp1_6000-exp0050" \
@@ -18,23 +18,23 @@ projects=(
           # "bcp1_6000-mp1_6000-exp0040" \
           # "bcp1_6000-mp1_6000-exp0050" \
           # "bcp1_6000-mp1_6000-exp0047" \
-        #   "bcp2_6000-bcp1_6000-exp0013" \
-        #   "bcp2_6000-bcp1_6000-exp0050" \
-        #   "bcp2_6000-bcp1_6000-exp0047" \
-        #   "bcp3_6000-bcp1_6000-exp0013" \
-        #   "bcp3_6000-bcp1_6000-exp0050" \
-        #   "bcp3_6000-bcp1_6000-exp0047" \
-        #   "bcp3_6000-bcp2_6000-exp0013" \
-        #   "bcp3_6000-bcp2_6000-exp0047" \
-        #   "bcp3_6000-bcp2_6000-exp0050" \
-        #   "pcp1_6000-bcp1_6000-exp0013" \
-        #   "pcp1_6000-bcp1_6000-exp0038" \
-        #   "pcp1_6000-bcp1_6000-exp0050" \
-        #   "pcp1_6000-bcp1_6000-exp0047" \
-        #   "mp1_6000-bcp1_6000-exp0013" \
-        #   "mp1_6000-bcp1_6000-exp0040" \
-        #   "mp1_6000-bcp1_6000-exp0050" \
-        #   "mp1_6000-bcp1_6000-exp0047"                    
+          # "bcp2_6000-bcp1_6000-exp0013" \
+          # "bcp2_6000-bcp1_6000-exp0050" \
+          # "bcp2_6000-bcp1_6000-exp0047" \
+          # "bcp3_6000-bcp1_6000-exp0013" \
+          # "bcp3_6000-bcp1_6000-exp0050" \
+          # "bcp3_6000-bcp1_6000-exp0047" \
+          # "bcp3_6000-bcp2_6000-exp0013" \
+          # "bcp3_6000-bcp2_6000-exp0047" \
+          # "bcp3_6000-bcp2_6000-exp0050" \
+          # "pcp1_6000-bcp1_6000-exp0013" \
+          # "pcp1_6000-bcp1_6000-exp0038" \
+          # "pcp1_6000-bcp1_6000-exp0050" \
+          # "pcp1_6000-bcp1_6000-exp0047" \
+          # "mp1_6000-bcp1_6000-exp0013" \
+          # "mp1_6000-bcp1_6000-exp0040" \
+          # "mp1_6000-bcp1_6000-exp0050" \
+          # "mp1_6000-bcp1_6000-exp0047"                    
           )
 
 
@@ -42,12 +42,12 @@ projects=(
 
 for i in ${projects[@]}; do
     # ref_query_auxilary
-    CUDA_VISIBLE_DEVICES=0 python gt_main.py --adj_training --data_dir=./experiments/$i  --use_auxilary --add_pos_enc
-    # CUDA_VISIBLE_DEVICES=0 python gt_main.py --data_dir=./experiments/$i  --use_auxilary
+    CUDA_VISIBLE_DEVICES=0 python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
+    # CUDA_VISIBLE_DEVICES=0 python gt_main.py --data_dir=./experiments/$i  --use_auxilary --add_pos_enc    
     # CUDA_VISIBLE_DEVICES=0 python gt_main.py --add_pos_enc --data_dir=./experiments/$i --use_auxilary
-    # CUDA_VISIBLE_DEVICES=0 python gt_main.py --active_learning --data_dir=./experiments/$i --use_auxilary
+    # CUDA_VISIBLE_DEVICES=0 python gt_main.py --active_learning --add_pos_enc --data_dir=./experiments/$i --use_auxilary
     # CUDA_VISIBLE_DEVICES=0 python gt_main.py --adj_training --data_dir=./experiments/$i --use_auxilary
-    # CUDA_VISIBLE_DEVICES=0 python gt_main.py --adj_training --add_pos_enc --active_learning --data_dir=./experiments/$i --use_auxilary
+    
     
     # ref_query
     # CUDA_VISIBLE_DEVICES=0 python gt_main.py --data_dir=./experiments/$i
