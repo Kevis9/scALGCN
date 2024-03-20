b16cbacfda43b8f2f5e120192546237d015ba944
kevislin <605684854@qq.com>
2024-03-20 15:05:48 +0800

    [bug]test

--- other_methods/deeplearning/scgcn/run_gcn.sh
+++ other_methods/deeplearning/scgcn/run_gcn.sh
@@ -92,7 +92,7 @@ length=${#projs[@]}
 export rgraph=FALSE
 export basepath="/home/hwl/scALGCN/experiments"
 
-for ((i = 0; i < length; i++)); do
+for ((i = 0; i < 1; i++)); do
     python h5_adaptor.py --dir_name=/home/hwl/scALGCN/experiments/${dir_name[i]} --proj=${projs[i]}
     Rscript data_preprocess.R $basepath/${dir_name[i]}/raw_data $rgraph
     python train_for_compare.py --proj ${projs[i]}    # 这里是tensorflow的参数传递方式不需要=
