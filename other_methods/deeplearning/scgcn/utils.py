32249333af755d561a6566699371bd283f592bd3
kevislin <605684854@qq.com>
2024-03-20 15:15:54 +0800

    [bug]numpy version for scGCN

--- other_methods/deeplearning/scgcn/utils.py
+++ other_methods/deeplearning/scgcn/utils.py
@@ -30,7 +30,7 @@ def sample_mask(idx, l):
     """Create mask."""
     mask = np.zeros(l)
     mask[idx] = 1
-    return np.array(mask, dtype=np.bool)
+    return np.array(mask, dtype=np.bool_)
 
 # convert nested lists to a flat list
 output = []
