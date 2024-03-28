0eefa67298e603ade70966eb155b91e1b50e1973
kevislin <605684854@qq.com>
2024-03-28 11:43:58 +0800

    [add] plot auxilary module

---
+++ random_sample_exp0050.py
@@ -0,0 +1,10 @@
+import anndata as ad
+
+sample_num = ['200', '400', '600', '800', '1000']
+
+adata = ad.read_h5ad('/home/Users/kevislin/raw_data/EXP0050/auxilary_data.h5ad')
+
+for num in sample_num:    
+    obs_indices = adata.obs_names[:num]    
+    sampled_adata = adata[obs_indices, :]
+    sampled_adata.write_h5ad(f'/home/Users/kevislin/raw_data/EXP0050/auxilary_data_{num}.h5ad')
