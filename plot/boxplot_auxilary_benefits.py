14eecc063f0f938282f81b5841902df5c7870967
kevislin <605684854@qq.com>
2024-03-28 10:51:59 +0800

    [add] plot auxilary module

--- plot/boxplot_auxilary_benefits.py
+++ plot/boxplot_auxilary_benefits.py
@@ -73,7 +73,7 @@ diff_type_no_aux = [
 ]
 
 acc_data = pd.read_csv('../result/stable_acc.csv', index_col=0)
-f1_data = pd.read_csv('../result/stable_f1.csv', index_col=0)
+f1_data = pd.read_csv('../result/stable_macro-f1.csv', index_col=0)
 
 same_type_with_aux_acc = acc_data.loc[same_type_with_aux]['GT + AL + GL']
 same_type_no_aux_acc = acc_data.loc[same_type_no_aux]['GT + AL + GL']
