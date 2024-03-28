f001c853b92ee6cd9270e2e53f90f1cf80ab7246
kevislin <605684854@qq.com>
2024-03-28 13:30:05 +0800

    [add] add correlation plot

--- plot/boxplot_auxilary_expdetail.py
+++ plot/boxplot_auxilary_expdetail.py
@@ -71,7 +71,7 @@ all_x = ['exp0013'] * len(exp0013_acc) + ['exp0050'] * len(exp0050_acc) + ['exp0
 
 data = pd.DataFrame({'val': all_val, 'x': all_x})
 
-sns.boxplot(data=data,x='x', y='val', showfliers=False, width=.3, palette='Set2')
+sns.boxplot(data=data,x='x', y='val', hue='x', showfliers=False, width=.3)
 plt.xlabel('')
 plt.ylabel('')
 plt.savefig('exp_detail_boxplot', dpi=300, transparent=True,bbox_inches="tight")
