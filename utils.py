32ec5672d753562949b1c80687f1b6a92d1b9c7e
linkhu <linkhu@tencent.com>
2024-04-30 11:55:13 +0800

    [bug] all random opreation check

--- utils.py
+++ utils.py
@@ -313,6 +313,7 @@ def get_data_info(args, adata, n_ref, n_query):
     if args.use_auxilary:
         auxilary_label = adata.uns['auxilary_label']
         idxs = [i for i in range(auxilary_label.shape[0])]
+        random.seed(32)
         random.shuffle(idxs)
         if args.auxilary_num != -1:
             idxs = idxs[:args.auxilary_num]                   
@@ -320,6 +321,7 @@ def get_data_info(args, adata, n_ref, n_query):
         data_info['auxilary_train_idx'] = auxilary_train_idx
         
 
+    random.seed(32)
     # 对val_idx再做一个split
     random.shuffle(val_idx)
     ration = 0.5
@@ -348,120 +350,6 @@ def get_auxilary_g_data(adata):
     return g_data
 
 
-def train(model, g_data, data_info, config):
-    '''
-        model: Graph transformer
-        g_data: DGLGraph        
-    '''
-    model.to(device)
-    g_data = g_data.to(device)
-    model.train()
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),
-                                 lr=config['para_config']['gt_lr'],
-                                 weight_decay=config['para_config']['wd'])
-
-    dense_adj = g_data.adjacency_matrix().to_dense().cpu().numpy()
-    graph = nx.Graph(dense_adj)
-    norm_centrality = centralissimo(graph)
-    # # 已选取的节点数目
-
-    max_val_acc = 0
-    tolerance_epoch = 0        
-    np.random.seed(20)
-    for epoch in range(config['para_config']['epochs']):
-        gamma = np.random.beta(1, 1.005 - config['para_config']['basef'] ** epoch)
-        alpha = beta = (1 - gamma) / 2
-        optimizer.zero_grad()
-
-        g_data = g_data.to(device)
-        node_x = g_data.ndata['x'].to(device)
-        lap_pe = g_data.ndata['PE'].to(device)
-
-        out = model(g_data, node_x, lap_pe)
-
-        # out = model(g_data, node_x, h_lap_pos_enc=lap_pe)
-
-        loss = criterion(out[data_info['train_idx']], g_data.ndata['y_predict'][data_info['train_idx']])
-        loss.backward()
-        optimizer.step()
-
-        prob = F.softmax(out.detach(), dim=1).cpu().numpy()
-
-        # prob是样本 * 类别
-        # 这里的entropy函数计算的是一列的信息熵，所以我们要转置一下，让一列成类，计算一个样本的信息熵
-        if config['para_config']['is_active_learning'] and len(data_info['train_idx']) < config['para_config']['NL']:
-            entropy = scipy.stats.entropy(prob.T)
-            kmeans = KMeans(n_clusters=data_info['NCL'], random_state=0).fit(prob)
-            ed_score = euclidean_distances(prob, kmeans.cluster_centers_)
-            density = np.min(ed_score, axis=1)
-            # entropy和density的norm: 计算样本中的百分位数（因为只需要比较样本之间的分数即可）
-            norm_entropy = np.array([perc_for_entropy(entropy, i) for i in range(len(entropy))])
-            norm_density = np.array([perc_for_density(density, i) for i in range(len(density))])
-            norm_centrality = norm_centrality.squeeze()
-            finalweight = alpha * norm_entropy + beta * norm_density + gamma * norm_centrality
-
-            # 把train, val, test的数据排除, 从剩余的label budget里面获取节点
-            finalweight[data_info['train_idx'] + data_info['test_idx'] + data_info['val_idx']] = -100
-            select_arr = np.argpartition(finalweight, -config['para_config']['k_select'])[-config['para_config']['k_select']:]
-            for node_idx in select_arr:
-                data_info['train_idx'].append(node_idx)
-                # 注意y_predict是tensor
-                g_data.ndata['y_predict'][node_idx] = g_data.ndata['y_true'][node_idx]
-                if (config['para_config']['debug']):
-                    print("Epoch {:}: pick up one node to the training set!".format(epoch))
-
-        # validation
-        model.eval()
-        out = model(g_data, node_x, lap_pe)
-        # 做一个detach
-        out = out.detach()
-        val_pred = torch.argmax(out[data_info['val_idx']], dim=1).cpu().numpy()
-        val_true = g_data.ndata['y_true'][data_info['val_idx']].cpu().numpy()
-        val_acc = accuracy_score(val_true, val_pred)
-        val_loss = criterion(out[data_info['val_idx']], g_data.ndata['y_true'][data_info['val_idx']])
-
-        test_pred = torch.argmax(out[data_info['test_idx']], dim=1).cpu().numpy()
-        test_true = g_data.ndata['y_true'][data_info['test_idx']].cpu().numpy()
-        test_acc = accuracy_score(test_pred, test_true)
-
-        train_pred = torch.argmax(out[data_info['train_idx']], dim=1).cpu().numpy()
-        train_true = g_data.ndata['y_true'][data_info['train_idx']].cpu().numpy()
-        train_acc = accuracy_score(train_pred, train_true)
-
-        if (config['para_config']['epoch_print_flag']):
-            print("Epoch {:}: traing loss: {:.3f}, train_acc: {:.3f}, val_loss {:.3f}, val_acc {:.3f}, test_acc {:.3f}".format(epoch, loss, train_acc, val_loss,
-                                                                                           val_acc, test_acc))
-        if (config['para_config']['early_stop']):
-            if max_val_acc < test_acc:
-                tolerance_epoch = 0
-                max_val_acc = test_acc
-                # save the model
-                torch.save(model.state_dict(),'result/{:}_model_acc_{:.3f}.pt'.format(config['data_config']['root'].split('/')[1], test_acc))                
-                torch.save(model.state_dict(), 'tmp_model.pt')
-            else:                
-                tolerance_epoch += 1
-                if tolerance_epoch > config['para_config']['tolerance_epoch']:
-                    print("Early stop at epoch {:}, return the max_val_acc model.".format(epoch))
-                    break
-            
-    # load saved model state_dict()
-    model = GTModel(config['para_config'])
-    model.load_state_dict(torch.load('tmp_model.pt'))    
-    model.to(device)        
-    return model
-
-
-def test(model, g_data, data_info):
-    model.eval()
-    g_data.to(device)
-    out = model(g_data.to(device), g_data.ndata["x"].to(device), g_data.ndata["PE"].to(device))
-    test_pred = torch.argmax(out[data_info['test_idx']], dim=1).cpu().numpy()
-    test_true = g_data.ndata['y_true'][data_info['test_idx']].cpu().numpy()
-    test_acc = accuracy_score(test_true, test_pred)
-    print("test acc {:.3f}".format(test_acc))
-    return test_acc
-
 def accuracy(output, labels):
     """Return accuracy of output compared to labels.
 
@@ -487,7 +375,7 @@ def accuracy(output, labels):
     return correct / len(labels)
 
 def active_learning(g_data, epoch, out_prob, norm_centrality, args, data_info):    
-    
+    np.random.seed(32)
     gamma = np.random.beta(1, 1.005 - args.basef ** epoch)
     alpha = beta = (1 - gamma) / 2
     prob = out_prob
